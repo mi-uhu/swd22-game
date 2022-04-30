3c040feb4c081537ad39546b11f1de5ae9feb9fc
michael <michael.unterhuber94@gmail.com>
2022-04-30 10:53:35 +0200

    changed main

--- core/src/main/java/at/compus02/swd/ss2022/game/Main.java
+++ core/src/main/java/at/compus02/swd/ss2022/game/Main.java
@@ -2,6 +2,8 @@ package at.compus02.swd.ss2022.game;
 
 import at.compus02.swd.ss2022.game.gameobjects.GameObject;
 import at.compus02.swd.ss2022.game.gameobjects.Sign;
+import at.compus02.swd.ss2022.game.gameobjects.Stone;
+import at.compus02.swd.ss2022.game.gameobjects.TileGras;
 import at.compus02.swd.ss2022.game.input.GameInput;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -14,66 +16,81 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
+ */
 public class Main extends ApplicationAdapter {
-	private SpriteBatch batch;
+    private SpriteBatch batch;
 
-	private ExtendViewport viewport = new ExtendViewport(480.0f, 480.0f, 480.0f, 480.0f);
-	private GameInput gameInput = new GameInput();
+    private ExtendViewport viewport = new ExtendViewport(480.0f, 480.0f, 480.0f, 480.0f);
+    private GameInput gameInput = new GameInput();
 
-	private Array<GameObject> gameObjects = new Array<>();
+    private Array<GameObject> gameObjects = new Array<>();
 
-	private final float updatesPerSecond = 60;
-	private final float logicFrameTime = 1 / updatesPerSecond;
-	private float deltaAccumulator = 0;
-	private BitmapFont font;
+    private final float updatesPerSecond = 60;
+    private final float logicFrameTime = 1 / updatesPerSecond;
+    private float deltaAccumulator = 0;
+    private BitmapFont font;
 
-	@Override
-	public void create() {
-		batch = new SpriteBatch();
-		gameObjects.add(new Sign());
-		font = new BitmapFont();
-		font.setColor(Color.WHITE);
-		Gdx.input.setInputProcessor(this.gameInput);
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        Sign sign = new Sign();
+        sign.setPosition(10, 10);
 
-	private void act(float delta) {
-		for(GameObject gameObject : gameObjects) {
-			gameObject.act(delta);
-		}
-	}
+//        for (int i = 0; i < 100; i++) {
+            TileGras tileGras = new TileGras();
+            tileGras.setPosition(-230, 200);
+            gameObjects.add(tileGras);
+            tileGras = new TileGras();
+            tileGras.setPosition(-198, 200);
+            gameObjects.add(tileGras);
+//        }
 
-	private void draw() {
-		batch.setProjectionMatrix(viewport.getCamera().combined);
-		batch.begin();
-		for(GameObject gameObject : gameObjects) {
-			gameObject.draw(batch);
-		}
-		font.draw(batch, "Hello Game", -220, -220);
-		batch.end();
-	}
+        gameObjects.add(sign);
+        gameObjects.add(new Stone());
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+        Gdx.input.setInputProcessor(this.gameInput);
+    }
 
-	@Override
-	public void render() {
-		Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    private void act(float delta) {
+        for (GameObject gameObject : gameObjects) {
+            gameObject.act(delta);
+        }
+    }
 
-		float delta = Gdx.graphics.getDeltaTime();
-		deltaAccumulator += delta;
-		while(deltaAccumulator > logicFrameTime) {
-			deltaAccumulator -= logicFrameTime;
-			act(logicFrameTime);
-		}
-		draw();
-	}
+    private void draw() {
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+        batch.begin();
+        for (GameObject gameObject : gameObjects) {
+            gameObject.draw(batch);
+        }
+        font.draw(batch, "Hello Game", -220, -220);
+        batch.end();
+    }
 
-	@Override
-	public void dispose() {
-		batch.dispose();
-	}
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-	@Override
-	public void resize(int width, int height){
-		viewport.update(width,height);
-	}
+        float delta = Gdx.graphics.getDeltaTime();
+        deltaAccumulator += delta;
+        while (deltaAccumulator > logicFrameTime) {
+            deltaAccumulator -= logicFrameTime;
+            act(logicFrameTime);
+        }
+        draw();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
 }
