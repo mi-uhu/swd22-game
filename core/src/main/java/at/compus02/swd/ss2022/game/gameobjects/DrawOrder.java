a5508ac2e11e7471dfa19af8bc828ade0d57e40e
Florian Hoelbling <s50721@edu.campus02.at>
2022-05-14 14:11:14 +0200

    change draw order

--- core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/DrawOrder.java
+++ core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/DrawOrder.java
@@ -3,12 +3,12 @@ package at.compus02.swd.ss2022.game.gameobjects;
 public class DrawOrder {
     public static final int LAVA = 0;
     public static final int WATER = 1;
-    public static final int GRAVEL = 2;
-    public static final int GRAS = 3;
-    public static final int TREASURE = 4;
-    public static final int WALL = 5;
-    public static final int STONE = 6;
-    public static final int LOG = 7;
+    public static final int GRAS = 2;
+    public static final int GRAVEL = 3;
+    public static final int WALL = 4;
+    public static final int STONE = 5;
+    public static final int LOG = 6;
+    public static final int TREASURE = 7;
     public static final int BUSH = 8;
     public static final int SIGN = 9;
 
