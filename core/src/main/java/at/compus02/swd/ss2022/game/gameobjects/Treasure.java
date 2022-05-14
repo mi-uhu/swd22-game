a664b33db2be5292805fa97c8085c3b7ee65bb8c
Christian <github.cg@gmail.com>
2022-05-14 13:00:47 +0200

    Treasure added

---
+++ core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/Treasure.java
@@ -0,0 +1,30 @@
+package at.compus02.swd.ss2022.game.gameobjects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Treasure implements GameObject {
+    private Texture image;
+    private Sprite sprite;
+
+    public Treasure() {
+        image = new Texture("treasurecl.png");
+        sprite = new Sprite(image);
+    }
+
+    @Override
+    public void act(float delta) {
+
+    }
+
+    @Override
+    public void setPosition(float x, float y) {
+        sprite.setPosition(x, y);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        sprite.draw(batch);
+    }
+}
